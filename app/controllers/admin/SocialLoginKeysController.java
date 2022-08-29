13825f52aaf498b5d45feffaa87d111591b53a52
gimartinez <gm@famly.co>
2022-08-30 00:38:31 +0200

    Implement errors for req 4

--- app/controllers/admin/SocialLoginKeysController.java
+++ app/controllers/admin/SocialLoginKeysController.java
@@ -3,6 +3,7 @@ package controllers.admin;
 import com.typesafe.config.Config;
 import controllers.WiFreeController;
 import controllers.routes;
+import models.Portal;
 import models.PortalLoginConfiguration;
 import play.data.Form;
 import play.mvc.Http;
@@ -17,8 +18,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -37,7 +40,31 @@ public class SocialLoginKeysController extends WiFreeController {
     public Result saveSocialKeys() {
         final Form<SocialKeysView> form = formFactory.form(SocialKeysView.class);
         final SocialKeysView socialKeys = form.bindFromRequest().get();
-        portalAndLoginOptionsService.saveLoginOptions(socialKeys, portalId());
-        return redirect(routes.AdminAppController.loginSettings());
+
+        List<String> errors = hasError(socialKeys.getFacebook(), "Facebook");
+        errors.addAll(hasError(socialKeys.getGoogle(), "Google"));
+        
+        for (String error : errors) {
+            flash("Error", error);
+        }
+        if (errors.isEmpty()) {
+            portalAndLoginOptionsService.saveLoginOptions(socialKeys, portalId());
+            return redirect(routes.AdminAppController.loginSettings());
+        } else {
+            return redirect(routes.AdminAppController.loginSettings());
+        }
+    }
+
+    private List<String> hasError(PortalLoginConfiguration p, String socialLogin) {
+        List<String> errors = new ArrayList<>();
+        if (p.isEnabled()) {
+            if (p.getKeys().getClientId().isEmpty()) {
+                errors.add("Por favor ingrese el 'clientId' provisto por " +socialLogin);
+            }
+            if (p.getKeys().getSecret().isEmpty()) {
+                errors.add("Por favor ingrese el 'tokenId' provisto por " + socialLogin);
+            }
+        }
+        return errors;
     }
 }
--- public/js/hideFormFields.js
+++ public/js/hideFormFields.js
@@ -12,5 +12,3 @@ function changeFields(target,fieldToHide){
     }
 }
 
-
-    
